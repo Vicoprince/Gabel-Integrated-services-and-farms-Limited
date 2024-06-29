9e48ffc6bde6af4751e58645bd1d03a54a9143b9
Vicoprince <oluajimativictor@gmail.com>
2024-06-29 16:49:41 +0100

    package changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production', // You can set this to 'production' for production builds
-  entry: './index.js', // Entry point of your application
+  entry: './app.js', // Entry point of your application
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build') // Output directory
